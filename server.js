e875af5484354cc56b5f345d7d0aaa9afb1b58e1
Julie <juliamwangi123>
2022-10-27 19:30:50 +0300

    simple express server

---
+++ server.js
@@ -0,0 +1,22 @@
+// import modules 
+require('dotenv').config()
+ const express = require('express');
+
+
+ //intialize express
+ const app = express()
+
+
+//get request
+app.get('/', (req,res)=>{
+    res.status(200).json({"slackUsername":"julia mwangi", "backend":"true", "age":"25", "bio": "My name is julia mwangi a  software developer from Kenya"})
+})
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log('connected')
+})
+ 
+
+
+
