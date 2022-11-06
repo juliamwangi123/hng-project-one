6ad667552ba5bc4249c28bdf51bd0ee487af5dcf
Julie <juliamwangi123>
2022-11-06 12:11:00 +0300

    operation

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 // import modules 
 require('dotenv').config()
  const express = require('express');
-
-
+const readline= require('readline-sync')
  //intialize express
  const app = express()
+ let operator =['+', '-', '*']
+
+ let pickedOperator= readline.question(`Select 0 for ${operator[0]} 1. For  ${operator[1]} or 2. for ${operator[2]}: `)
 
 
-//get request
-app.get('/', (req,res)=>{
-    res.status(200).json({"slackUsername":"julia mwangi", "backend":true, "age":25, "bio": "My name is julia mwangi a  software developer from Kenya"})
+ let opertion =()=>{
+
+    let num1 = Number(readline.question('input value 1:  '))
+    let num2 = Number(readline.question('input value 2:  ')
+)
+
+    if(pickedOperator == operator.indexOf('+')){
+        return num1 + num2
+    }else if( pickedOperator == operator.indexOf('-')){
+        return num1 - num2
+    }else if(pickedOperator == operator.indexOf('*')){
+        return num1 * num2
+    }else{
+        return 'invalid operation'
+    }
+
+
+
+ }
+
+
+
+
+app.post('/', (req, res)=>{
+    
+let results=opertion()
+res.status(200).json({"slackUsername": "julia mwangi","result" : results, "operation-type":operator[pickedOperator]})
+
+
 })
 
-
 app.listen(process.env.PORT, ()=>{
-    console.log('connected')
+
 })
 
 // Export the Express API
