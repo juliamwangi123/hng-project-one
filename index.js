76ecad467994c91e3688fcb276d055089939bd89
Julie <juliamwangi123>
2022-11-06 21:18:22 +0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express  = require("express");
+const indexRouter =require('./routes/routes')
 
-const indexRouter = require("./routes/routes");
 
 const app = express();
 
-// app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use(indexRouter);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,38 @@
 const express = require("express");
-const assignmentController = require("../controller/controller");
 const router = express.Router();
 
-router.get("/", assignmentController.index);
-router.post("/", assignmentController.operation);
+router.get('/', (rqq,res)=>{
+    res.status(200).json({"slackUsername":"julia mwangi", age:25, backend:true, bio:"am a software developer"})
+})
+
+
+router.post("/", (req, res)=>{
+    let x =Number(req.body.x)
+    let y = Number(req.body.y)
+    let results;
+    let operation_type;
+
+    switch(operation_type){
+        case 'addition':
+            results= x+y;
+            break;
+        case 'subtraction':
+                results= x+y;
+                break
+        case 'multiplication':
+                    results= x+y;
+                    break
+        default:
+            return 'invalid operation'
+    }
+
+    res.status(200).json({slackUsername: "julia mwangi", result:results,  operation_type:operation_type})
+    
+
+
+});
+
+
+
 
 module.exports = router;
